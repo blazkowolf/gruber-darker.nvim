94c369e1c2ad9f156bf5f8b3d64edf9ceb1d4f55
Daniel Hill <danielhill10@bellsouth.net>
2023-01-05 22:04:18 -0500

    chore: add more colors

--- lua/gruber-darker/colors.lua
+++ lua/gruber-darker/colors.lua
@@ -1,5 +1,6 @@
 local M = {}
 
+---@class Palette
 M.default = {
   none = "NONE",
   fg = "#e4e4e4",
@@ -13,7 +14,19 @@ M.default = {
   ["bg+2"] = "#453d41",
   ["bg+3"] = "#484848",
   ["bg+4"] = "#52494e",
+  ["red-1"] = "#c73c3f",
+  red = "#f43841",
+  ["red+1"] = "#ff4f58",
+  green = "#73d936",
+  yellow = "#ffdd33",
+  brown = "#cc8c3c",
+  quartz = "#95a99f",
+  ["niagara-2"] = "#303540",
+  ["niagara-1"] = "#565f73",
+  niagara = "#96a6c8",
+  wisteria = "#9e95c7",
 }
 
 return M
+
 
