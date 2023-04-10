8605e522797b8e064ef2e32a2a46a2f139e1b83c
Daniel Hill <9439488+blazkowolf@users.noreply.github.com>
2023-04-10 13:20:54 -0400

    feat: add correct cursor color

--- lua/gruber-darker/highlights/init.lua
+++ lua/gruber-darker/highlights/init.lua
@@ -2,6 +2,7 @@ local M = {}
 
 ---@class HighlightsProvider
 ---@field highlights table<string, Highlight>
+---@field setup fun()
 
 ---@type HighlightsProvider[]
 local providers = {
@@ -17,6 +18,7 @@ function M.setup()
 	for _, provider in ipairs(providers) do
 		provider:setup()
 	end
+  vim.opt.guicursor:append("a:Cursor/lCursor")
 end
 
 return M
